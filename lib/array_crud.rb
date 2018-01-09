d2b85d5523313b9998114c43f05c7c26160fcfee
Gil Sunshine <gilsunshine@gmail.com>
2018-01-09 15:28:15 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.at(index_number)
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
-  array.first
+  array[0]
 end
 
 def retrieve_last_element_from_array(array)
-  array.last
+  array[-1]
 end
